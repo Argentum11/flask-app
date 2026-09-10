aa4f26dc848145c641284c6ad9ef0406649e1711
Argentum11 <92793837+Argentum11@users.noreply.github.com>
2026-09-10 10:20:48 +0800

    feat(template): render template for / route

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object("config.DevelopmentConfig")
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html", username="john")
 
 if __name__ == "__main__":
     app.run(port=8081)
